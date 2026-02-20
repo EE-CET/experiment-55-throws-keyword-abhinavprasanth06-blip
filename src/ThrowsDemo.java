6d26943315406dbe610901fb129871d9e01b26f8
Your NameAbhinav <abhinavprasanth06@gmail.com>
2026-02-20 05:55:45 +0530

    added this

--- src/ThrowsDemo.java
+++ src/ThrowsDemo.java
@@ -1,19 +1,28 @@
+
 import java.util.Scanner;
 import java.io.IOException;
 
-public class ThrowsDemo {
-    
-    // TODO: Define static method check(int n) that throws IOException
-    // If n < 0, throw new IOException()
-    // Else, print n
+public class ThrowsDemo{
+	public  void check(int num){
+	    try{
+    	    if(num<0){
+    	        throw new IOException();
+    	    }
+    	    else{
+    	        	   
+	        System.out.println(num);
+    	    }
+	    }
+	    catch(IOException  e){
+	        System.out.println( "Caught: IO Exception");
+	    }
 
-   
-        // TODO: Read n
-        
-        // TODO: try {
-        //     call check(n)
-        // } catch (IOException e) {
-        //     Print "Caught: IO Exception"
-        // }
-    
+	}
+     public static void main (String[] args) {
+        ThrowsDemo d1 = new ThrowsDemo();
+        Scanner sc = new Scanner(System.in);
+        int inp = sc.nextInt();
+        d1.check(inp);
+    }
 }
+		
